6dcce3c895d77b28e56f499df814a33b3653f25f
Hundred <caseypaulinfox@gmail.com>
2026-03-21 11:28:40 +0000

    Update file writing logic in python.py
    Refactor file writing to append mode and define save function.

--- Life Visuliser/python.py
+++ Life Visuliser/python.py
@@ -1,6 +1,14 @@
 import json
 
 
-with open(r"C:\Users\trevo\OneDrive\Documents\C# thing\a new personal winform folder\Life Visuliser\Life Visuliser\bin\Debug\text.txt","w") as file:
-    lines = "testing"
-    file.writelines(lines)
+file_path = r"C:\Users\trevo\OneDrive\Documents\C# thing\a new personal winform folder\Life Visuliser\Life Visuliser\bin\Debug\text.txt"
+
+#save 
+
+def save():
+    with open(file_path, "a") as f:
+        lines = "testing2\n"
+        f.write(lines)
+
+        
+    
